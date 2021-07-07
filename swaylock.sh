c297094ec685bfc777244e8ea1ef6a1841bbb249
FabseGP <59748123+FabseGP@users.noreply.github.com>
2021-07-07 12:55:21 +0200

    Delete swaylock.sh

--- swaylock.sh
+++
@@ -1,9 +0,0 @@
-#cs!/bin/bash
-
-echo begining of the script
-
-image="/tmp/swaylockscreen.jpeg"
-
-grim -t jpeg -q 1 $image && ffmpeg -i $image -filter_complex boxblur=lr=5:lp=2 -y $image
-
-swaylock -i "$image" -n

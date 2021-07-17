455a34c8f8e7a65976ab0ba0297b4c81270dba5c
FabseGP <59748123+FabseGP@users.noreply.github.com>
2021-07-17 11:15:21 +0200

    Update external_harddrive_mount.sh

--- Aftermath/external_harddrive_mount.sh
+++ Aftermath/external_harddrive_mount.sh
@@ -1,12 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-USERNAME="brugernavn"
-FILESYSTEM_TYPE="btrfs"
+# Parameters
+
+  FILESYSTEM_TYPE="btrfs"
+
+#----------------------------------------------------------------------------------------------------------------------------------
 
 # Mappe til mount
-MOUNT="/media/SEAGATE"
-mkdir -p $MOUNT
+
+  MOUNT="/media/SEAGATE"
+  mkdir -p "$MOUNT"
+
+#----------------------------------------------------------------------------------------------------------------------------------
 
 # fstab
-echo "/dev/sdb4 $MOUNT $FILESYSTEM_TYPE defaults,noatime 0 2" >> /etc/fstab
+
+  echo "/dev/sdb4 "$MOUNT" "$FILESYSTEM_TYPE" defaults,noatime 0 2" >> sudo /etc/fstab
  

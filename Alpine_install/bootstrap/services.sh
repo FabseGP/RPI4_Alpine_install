98ebf2900bd534f92eeb58e450399756b45bf9ee
FabseGP <59748123+FabseGP@users.noreply.github.com>
2021-07-17 11:08:46 +0200

    Update services.sh

--- Alpine_install/bootstrap/services.sh
+++ Alpine_install/bootstrap/services.sh
@@ -1,21 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
-set -xe
+# Parameters
 
-apk add openssh haveged dbus openntpd
 
-for service in devfs dmesg mdev; do
-	rc-update add $service sysinit
-done
 
-for service in modules sysctl hostname bootmisc swclock syslog swap; do
-	rc-update add $service boot
-done
+#----------------------------------------------------------------------------------------------------------------------------------
 
-for service in dbus haveged sshd openntpd local networking networkmanager bluetooth wpa_supplicant; do
-	rc-update add $service default
-done
+# The fun part
 
-for service in mount-ro killprocs savecache; do
-	rc-update add $service shutdown
-done
+  set -xe
+
+  apk add openssh haveged dbus openntpd
+
+  for service in devfs dmesg mdev; do
+    rc-update add $service sysinit
+  done
+
+  for service in modules sysctl hostname bootmisc swclock syslog swap; do
+    rc-update add $service boot
+  done
+
+  for service in dbus haveged sshd openntpd local networking networkmanager bluetooth wpa_supplicant; do
+    rc-update add $service default
+  done
+
+  for service in mount-ro killprocs savecache; do
+    rc-update add $service shutdown
+  done
